b4652a0dbf718a1b2f052bb84f03fd9031397894
Luana <luanaa.domeneghettii@gmail.com>
2024-10-10 22:19:33 -0300

    feat: create method get

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -8,23 +8,28 @@ let suspeitos = [
     id: Math.floor(Math.random() * 1000000),
     nome: "Beyonce",
     idade: 43,
-    envolvimento: "sim",
+    envolvimento: true,
     descricaoFisica: [ "Loira", "Pele Morena", "Alta" ],
 },
 {
     id: Math.floor(Math.random() * 1000000),
     nome: "Travis Scott",
     idade: 33,
-    envolvimento: "não",
+    envolvimento: false,
     descricaoFisica: [ "Tatuado", "Pele Negra", "cabelo crespo" ],
 },
 {
     id: Math.floor(Math.random() * 1000000),
     nome: "Drake",
     idade: 37,
-    envolvimento: "sim",
+    envolvimento: true,
     descricaoFisica: [ "Cabelo Crespo", "Pele Branca", "Alto", "Tatuado"],
 },
 ];
 
+// Rota para listar todos os suspeitos cadastrados
+suspeitosRoutes.get("/", (req, res) => {
+    return res.status(200).json(suspeitos);
+  });
+  
 export default suspeitosRoutes;
